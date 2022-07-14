0a34b05477193193a556cfcea652fa52b2fe0096
Marshmelooo97 <91906479+Marshmelooo97@users.noreply.github.com>
2022-07-14 18:49:33 +0200

    B

--- ÜbungLernen/main.js
+++ ÜbungLernen/main.js
@@ -13,11 +13,53 @@ document.getElementById("tabelle").addEventListener("click", () => {
     for (let number of arr){
         document.write(number)
     }
-
+    document.write("sdfgsdfsdf")
     var test = JSON.parse("json.json")
     document.write(test)
-
-
-
+    document.write("12313243123")
 
 })
+
+class Hund{
+    constructor(name,age){
+        this.name = name
+        this.age = age
+    }
+    getDog(){
+        document.write(this.age + this.name)
+    }
+}
+
+document.getElementById("klasse").addEventListener("click", function(e){
+    let name = document.getElementById("name").value
+    let dog = new Hund(name,10)
+    dog.getDog()
+
+})
+
+class Form{
+    constructor(farbe){this.farbe = farbe}
+    info(){console.log("Falsch")}}
+class Rechteck extends Form{
+    constructor(farbe, höhe,breite){
+        super(farbe)
+        this.höhe = höhe
+        this.breite = breite}
+    fläche(){console.log(this.höhe*this.breite)}
+    info(){console.log("Farbe ist: " + this.farbe)}}
+
+document.getElementById("form").addEventListener("click", function(e){
+    let höhe = document.getElementById("höhe").value
+    let breite = document.getElementById("breite").value
+    let form1 = new Rechteck("r",höhe,breite,1,2)
+    form1.fläche()
+    form1.info()
+})
+
+let sTest = ["O","T","T","O"]
+
+let funkPol = function(sarr){
+    if(sarr[i] == sarr[sarr.lenght -i]){
+
+    }
+}
